5ba663a32e5b26f4ff05b66cf25c1df10f0cfb3a
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-24 17:23:30 -0800

    Refactor out Nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-        <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-        <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
-        <ul className="navbar-nav px-3">
-          <li className="nav-item text-nowrap">
-            <a className="nav-link" href="#">Sign out</a>
-          </li>
-        </ul>
-      </nav>
+      <Nav />
 
       <div className="container-fluid">
         <div className="row">
