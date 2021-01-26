a29aa86738d0333a6b42452aaee4629bd94bd809
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-25 16:37:42 -0800

    Clean up login form

--- src/public/Login.tsx
+++ src/public/Login.tsx
@@ -10,13 +10,7 @@ class Login extends Component {
         <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
         <label htmlFor="inputPassword" className="sr-only">Password</label>
         <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-        <div className="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me" /> Remember me
-        </label>
-        </div>
         <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <p className="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
       </form>
     );
   }
