195895092db72536ebe01bfc5076af9f8cb6958a
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-25 16:34:41 -0800

    Refactor Dashboard and Users component with parent Wrapper component that shows navs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Nav from './secure/components/Nav';
-import Menu from './secure/components/Menu';
 import Dashboard from './secure/Dashboard';
 import Users from './secure/Users';
 import { BrowserRouter, Route } from 'react-router-dom';
@@ -11,21 +9,11 @@ import Login from './public/Login';
 function App() {
   return (
     <div className="App">
-      <Nav />
-
-      <div className="container-fluid">
-        <div className="row">
-          <Menu />
-
-          <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-            <BrowserRouter>
-              <Route path={'/'} exact component={Dashboard} />
-              <Route path={'/users'} component={Users} />
-              <Route path={'/login'} component={Login} />
-            </BrowserRouter>
-          </main>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Route path={'/'} exact component={Dashboard} />
+        <Route path={'/users'} component={Users} />
+        <Route path={'/login'} component={Login} />
+      </BrowserRouter>
     </div>
   );
 }
--- src/secure/Dashboard.tsx
+++ src/secure/Dashboard.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Wrapper from './Wrapper';
 
 
 const Dashboard = () => (
-  <div>
+  <Wrapper>
     <h2>Section title</h2>
 
     <div className="table-responsive">
@@ -27,7 +28,7 @@ const Dashboard = () => (
         </tbody>
       </table>
     </div>
-  </div>
+  </Wrapper>
 );
 
 export default Dashboard;
