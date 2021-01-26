4ca928bee6069cbf1b6b8a4e4b16ba2ac415e1e7
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-25 16:58:39 -0800

    Add Register form component

---
+++ src/public/Register.tsx
@@ -0,0 +1,59 @@
+import React, { Component, SyntheticEvent } from 'react';
+import './Public.css';
+
+class Register extends Component {
+  firstName = '';
+  lastName = '';
+  email = '';
+  password = '';
+  passwordConfirm = '';
+
+  submit = (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    console.log({
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      password: this.password,
+      passwordConfirm: this.passwordConfirm
+    });
+  }
+
+  render() {
+    return (
+      <form className="form-signin" onSubmit={this.submit}>
+        <h1 className="h3 mb-3 font-weight-normal">Please register</h1>
+
+        <label htmlFor="firstName" className="sr-only">First Name</label>
+        <input type="text" id="firstName" className="form-control" placeholder="First Name" required
+          onChange={e => this.firstName = e.target.value}
+        />
+
+        <label htmlFor="lastName" className="sr-only">Last Name</label>
+        <input type="text" id="lastName" className="form-control" placeholder="Last Name" required
+          onChange={e => this.lastName = e.target.value}
+        />
+
+        <label htmlFor="inputEmail" className="sr-only">Email address</label>
+        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required
+          onChange={e => this.email = e.target.value}
+        />
+
+        <label htmlFor="inputPassword" className="sr-only">Password</label>
+        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required
+          onChange={e => this.password = e.target.value}
+        />
+
+        <label htmlFor="passwordConfirm" className="sr-only">Password Confirm</label>
+        <input type="password" id="passwordConfirm" className="form-control" placeholder="Password Confirm" required
+          onChange={e => this.passwordConfirm = e.target.value}
+        />
+
+        <button className="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
+      </form>
+    );
+  }
+}
+
+export default Register;
