4aba32c3bed2afc53f1e6169d2f8829f4faa3a78
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-27 20:00:29 -0800

    Render a list of users fetched from API inside of the table

--- src/secure/users/Users.tsx
+++ src/secure/users/Users.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Wrapper from '../Wrapper';
+import axios from 'axios';
+import { User } from '../../classes/user';
 
-const Users = () => (
-  <Wrapper>
-    <p>Users works!</p>
-  </Wrapper>
-);
+class Users extends Component {
+  state = { users: [] };
+  componentDidMount = async () => {
+    const response = await axios.get('/users');
+    this.setState({
+      users: response.data.data,
+    });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <div className="table-responsive">
+          <table className="table table-striped table-sm">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.users.map((user: User) => {
+                return (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>
+                      {user.first_name} {user.last_name}
+                    </td>
+                    <td>{user.email}</td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </Wrapper>
+    );
+  }
+}
 
 export default Users;
