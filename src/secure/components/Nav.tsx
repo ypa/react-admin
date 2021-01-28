000efff9555263f0b99a73c9bb73d2db07dce5ac
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-27 19:38:59 -0800

    Handle user sign out

--- src/secure/components/Nav.tsx
+++ src/secure/components/Nav.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
-const Nav = () => (
-    <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-        <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+class Nav extends Component {
+  state = { redirect: false };
+
+  handleSignOut = async () => {
+    await axios.post('/logout', {});
+
+    this.setState({
+      redirect: true,
+    });
+  };
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/login" />;
+    }
+
+    return (
+      <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+        <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
+          Company name
+        </a>
         <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap">
-                <a className="nav-link" href="#">Sign out</a>
-            </li>
+          <li className="nav-item text-nowrap">
+            <a className="nav-link" href="#" onClick={this.handleSignOut}>
+              Sign out
+            </a>
+          </li>
         </ul>
-    </nav>
-);
+      </nav>
+    );
+  }
+}
 
 export default Nav;
